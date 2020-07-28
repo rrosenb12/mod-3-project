06f9a302f9e59324ed95769c0033235edcb23ab0
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-28 14:01:34 -0400

    basic functionality for rendering a quiz

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", (e) => {
 
 const quizzesContainer = document.getElementById('quizzes-container')
+const displayQuiz = document.getElementById('display-quiz')
 let quizzesArray
 
 const fetchQuizzes = () => {
@@ -11,24 +12,26 @@ const fetchQuizzes = () => {
         quizzes.forEach(quiz => renderQuizzes(quiz)) })
 }
 
-function renderQuizzes(quiz){
+const renderQuizzes = (quiz) => {
     const quizSpan = document.createElement('span')
     quizSpan.innerText = quiz.title
     quizSpan.dataset.id = quiz.id
     quizSpan.className = 'quiz-span'
     quizzesContainer.append(quizSpan)
     quizSpan.addEventListener('click', function(e){
-        renderQuiz(e, quiz)
+        getQuiz(e)
     })
 }
 
-const renderQuiz = (e, quiz) => {
-console.log(e.target)
+const getQuiz = (e) => {
+    const currentQuizId = e.target.dataset.id
+    const quizNow = quizzesArray.find(quiz => quiz.id == currentQuizId)
+    renderQuiz(quizNow)
 }
 
-// function renderQuiz(quiz){
-//      const quizEl
-// }
+const renderQuiz = (quizNow) => {
+    console.log(quizNow)
+}
 
 fetchQuizzes();
 
