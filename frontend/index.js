1c326ee630fd52f39aef54251e3c77ae19e2c0e8
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-28 15:28:27 -0400

    Done.

--- frontend/index.js
+++ frontend/index.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
 const quizzesContainer = document.getElementById('quizzes-container')
 const displayQuiz = document.getElementById('display-quiz')
 let quizzesArray
+let questionsArray
+const questionsContainer = document.createElement('ul')
+
 
 const fetchQuizzes = () => {
     fetch(`http://localhost:3000/api/v1/quizzes`)
@@ -19,11 +22,13 @@ const renderQuizzes = (quiz) => {
     quizSpan.className = 'quiz-span'
     quizzesContainer.append(quizSpan)
     quizSpan.addEventListener('click', (e) => {
+        questionsContainer.innerHTML = ``
         getQuiz(e)
     })
 }
 
 const getQuiz = (e) => {
+
     const currentQuizId = e.target.dataset.id
     const quizNow = quizzesArray.find(quiz => quiz.id == currentQuizId)
     fetchQuestions(quizNow)
@@ -32,18 +37,40 @@ const getQuiz = (e) => {
 const fetchQuestions = (quizNow) => {
     fetch('http://localhost:3000/api/v1/questions')
     .then(response => response.json())
-    .then(questionsArray => getQuizQuestions(questionsArray, quizNow))
+    .then(questionsObj => {
+        questionsArray = questionsObj,
+        getQuizQuestions(questionsArray, quizNow)})
 }
 
 const getQuizQuestions = (questionsArray, quizNow) => {
     const quizQuestions = questionsArray.filter(question => question.quiz_id == quizNow.id)
-    quizQuestions.forEach(question => renderQuestions(question))
+    quizQuestions.forEach(question => {
+        fetchAnswers(question)
+    })
 }
 
-const renderQuestions = (question) => {
-    const questionsContainer = document.createElement('ul')
+const fetchAnswers = (question) => {
+    fetch('http://localhost:3000/api/v1/answers')
+    .then(response => response.json())
+    .then(answersArray => getAnswers(question, answersArray))
+}
+
+const getAnswers = (question, answersArray) => {
+    const questionAnswers = answersArray.filter(answer => answer.question_id == question.id)
+    renderQuestions(question, questionAnswers)
+}
+
+const renderQuestions = (question, questionAnswers) => {
     const questionLi = document.createElement('li')
     questionLi.innerText = question.content
+    
+    questionAnswers.forEach(answer => {
+        const questionAnswer = document.createElement('p')
+        questionAnswer.innerText = answer.answer_content
+        questionAnswer.className = 'the-questions-answer'
+        questionLi.append(questionAnswer)
+    })
+    
     questionsContainer.append(questionLi)
     displayQuiz.append(questionsContainer)
 }
