8fdc4dcf1ac64e234c8aa5a8862fb170fb052a86
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-28 14:55:49 -0400

    more indexes

--- personality_quiz/app/controllers/api/v1/answers_controller.rb
+++ personality_quiz/app/controllers/api/v1/answers_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::AnswersController < ApplicationController
+
+    def index
+        answers = Answer.all
+
+        render json: answers   
+    end
+
 end
--- personality_quiz/app/controllers/api/v1/questions_controller.rb
+++ personality_quiz/app/controllers/api/v1/questions_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::QuestionsController < ApplicationController
+
+    def index
+        questions = Question.all
+
+        render json: questions   
+    end
+
 end
