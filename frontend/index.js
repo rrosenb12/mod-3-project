412e6b15ee220d4b534a4cc47f9b42ed4641b0ac
mstaso <mstaso1@gmail.com>
2020-07-28 17:33:54 -0400

    attempted submit button

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,7 @@ const displayQuiz = document.getElementById('display-quiz')
 let quizzesArray
 let questionsArray
 const questionsContainer = document.createElement('ul')
+questionsContainer.className = 'questions-container'
 
 
 const fetchQuizzes = () => {
@@ -39,25 +40,28 @@ const fetchQuestions = (quizNow) => {
     .then(response => response.json())
     .then(questionsObj => {
         questionsArray = questionsObj
-        getQuizQuestions(questionsArray, quizNow)})
+        fetchAnswers(questionsArray, quizNow)})
 }
 
-const getQuizQuestions = (questionsArray, quizNow) => {
-    const quizQuestions = questionsArray.filter(question => question.quiz_id == quizNow.id)
-    quizQuestions.forEach(question => {
-        fetchAnswers(question)
-    })
-}
+// const getQuizQuestions = (questionsArray, quizNow) => {
+//     const quizQuestions = questionsArray.filter(question => question.quiz_id == quizNow.id)
+//     quizQuestions.forEach(question => {
+//         fetchAnswers(question)
+//     })
+// }
 
-const fetchAnswers = (question) => {
+const fetchAnswers = (questionsArray, quizNow) => {
     fetch('http://localhost:3000/api/v1/answers')
     .then(response => response.json())
-    .then(answersArray => getAnswers(question, answersArray))
+    .then(answersArray => getAnswers(questionsArray, quizNow, answersArray))
 }
 
-const getAnswers = (question, answersArray) => {
-    const questionAnswers = answersArray.filter(answer => answer.question_id == question.id)
+const getAnswers = (questionsArray, quizNow, answersArray) => {
+    const quizQuestions = questionsArray.filter(question => question.quiz_id == quizNow.id)
+    quizQuestions.forEach(question => {
+        const questionAnswers = answersArray.filter(answer => answer.question_id == question.id)
     renderQuestions(question, questionAnswers)
+    })    
 }
 
 const renderQuestions = (question, questionAnswers) => {
@@ -71,14 +75,19 @@ const renderQuestions = (question, questionAnswers) => {
         questionAnswer.className = 'the-questions-answer'
         questionLi.append(questionAnswer)
         
+        
+
         questionAnswer.addEventListener('click', function(e){
              addValue(e)
                
             
         })
     })
-    questionsContainer.append(questionLi)
-    displayQuiz.append(questionsContainer)   
+    const submitButton = document.createElement('button')
+    submitButton.innerText = 'Submit'
+    questionsContainer.append(questionLi, submitButton)
+    displayQuiz.append(questionsContainer)
+
     
 }
 let valueArray = []
