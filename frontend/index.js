dc3f106fd6cc8bf82bfb4ea8a5ce1deb44d7f45c
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-28 14:56:07 -0400

    rendered questions for specific quiz

--- frontend/index.js
+++ frontend/index.js
@@ -37,11 +37,15 @@ const fetchQuestions = (quizNow) => {
 
 const getQuizQuestions = (questionsArray, quizNow) => {
     const quizQuestions = questionsArray.filter(question => question.quiz_id == quizNow.id)
-    renderQuestions(quizQuestions)
+    quizQuestions.forEach(question => renderQuestions(question))
 }
 
-const renderQuestions = (quizQuestions) => {
-    console.log(quizQuestions)
+const renderQuestions = (question) => {
+    const questionsContainer = document.createElement('ul')
+    const questionLi = document.createElement('li')
+    questionLi.innerText = question.content
+    questionsContainer.append(questionLi)
+    displayQuiz.append(questionsContainer)
 }
 
 fetchQuizzes();
