bbe09fac257d864f89e30e9c15c2c57ad1751a7a
mstaso <mstaso1@gmail.com>
2020-07-29 10:13:57 -0400

    added index and show for Result

--- personality_quiz/app/controllers/api/v1/results_controller.rb
+++ personality_quiz/app/controllers/api/v1/results_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::ResultsController < ApplicationController
+
+    def index
+        results = Result.all
+
+        render json: results   
+    end
+
+    def show
+        result = Result.find(params[:id])
+
+        render json: result   
+    end
+
 end
