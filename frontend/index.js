c09be58bf99a82e3a2e021b4f6329fc2358fbd03
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-07-29 21:30:42 -0400

    two issues but wrote htem down

--- frontend/index.js
+++ frontend/index.js
@@ -1,113 +1,129 @@
 document.addEventListener("DOMContentLoaded", (e) => {
 
-    const quizzesContainer = document.getElementById('quizzes-container')
-    const displayQuiz = document.getElementById('display-quiz')
-    const displayResult = document.getElementById('display-result')
-
-    const questionsContainer = document.createElement('ul')
-    questionsContainer.className = 'questions-container'
-
-    const questionsDiv = document.createElement('div')
-    questionsDiv.className = 'questions-div'
-
-    const submitButton = document.createElement('button')
-    submitButton.innerText = 'Submit'
-
-    const likeButton = document.createElement('button')
-    likeButton.innerText = 'Like This Quiz'
-
-    // let likes = document.createElement('p')
-
-    let quizzesArray
-    let questionsArray
-    let valuesArray = []
+    const quizzesContainer = document.getElementById('quizzes-container');
+    const displayQuiz = document.getElementById('display-quiz');
+    const displayResult = document.getElementById('display-result');
+    const quizContentContainer = document.getElementById('quiz-content-container');
+    const likes = document.createElement('span');
+    const questionsDiv = document.createElement('div');
+    questionsDiv.className = 'questions-div';
+    const submitButton = document.createElement('button');
+    submitButton.innerText = 'Submit';
+    const likeButton = document.createElement('button');
+    likeButton.innerText = 'Like This Quiz';
+    let quizzesArray;
+    let questionsArray;
+    let answersArray;
+    let valuesArray = [];
 
     const fetchQuizzes = () => {
         fetch(`http://localhost:3000/api/v1/quizzes`)
         .then(response => response.json())
-        .then(quizzes => {
-            quizzesArray = quizzes 
-            quizzes.forEach(quiz => renderQuizzes(quiz)) }
-        )
+        .then(quizzesObj => {
+            quizzesArray = quizzesObj
+            const sortedQuizzes = quizzesObj.sort((a,b) => a.id - b.id)
+            sortedQuizzes.forEach(quiz => renderQuizzes(quiz))
+        })
     }
-
+    
     const renderQuizzes = (quiz) => {
-        const quizSpan = document.createElement('span')
-        quizSpan.innerText = quiz.title
-        quizSpan.dataset.id = quiz.id
-        quizSpan.className = 'quiz-span'
-        quizzesContainer.append(quizSpan)
+        const quizSpan = document.createElement('span');
+        quizSpan.innerText = quiz.title; 
+        quizSpan.dataset.id = quiz.id; 
+        quizSpan.className = 'quiz-span';
+        quizzesContainer.append(quizSpan);
         quizSpan.addEventListener('click', (e) => {
-            questionsContainer.innerHTML = ``
-            displayQuiz.innerHTML = ``
-            getQuiz(e)
+            quizContentContainer.innerHTML = ``;
+            displayResult.innerHTML = ``;
+            likes.innerHTML = ``;
+            valuesArray = [];
+            const currentQuiz = quizzesArray.find(quiz => quiz.id == e.target.dataset.id);
+            likes.textContent = currentQuiz.likes;
+            likeButton.dataset.id = currentQuiz.id;
+            displayQuiz.append(submitButton, likeButton, likes);
+            getQuiz(e);
         })
     }
 
     const getQuiz = (e) => {
-        const currentQuizId = e.target.dataset.id
-        const quizNow = quizzesArray.find(quiz => quiz.id == currentQuizId)
-        fetchQuestions(quizNow)
+        const currentQuizId = e.target.dataset.id;
+        const quizNow = quizzesArray.find(quiz => quiz.id == currentQuizId);
+        getQuestions(quizNow);
     }
 
-    const fetchQuestions = (quizNow) => {
+    const fetchQuestions = () => {
         fetch('http://localhost:3000/api/v1/questions')
         .then(response => response.json())
-        .then(questionsObj => {
-            questionsArray = questionsObj
-            fetchAnswers(questionsArray, quizNow)}
-        )
+        .then(questionsObj => questionsArray = questionsObj)
     }
 
-    const fetchAnswers = (questionsArray, quizNow) => {
+    const fetchAnswers = () => {
         fetch('http://localhost:3000/api/v1/answers')
         .then(response => response.json())
-        .then(answersArray => getQuestions(questionsArray, quizNow, answersArray))
+        .then(answersObj => answersArray = answersObj)
     }
 
-    const getQuestions = (questionsArray, quizNow, answersArray) => {
-        const quizQuestions = questionsArray.filter(question => question.quiz_id == quizNow.id)
-        let likes = document.createElement('p')
-        likes.dataset.id = quizNow.id
-        if (quizNow.likes === 0) {
-            likes.innerHTML = `<span>0</span> likes`
-        } else {
-            likes.innerHTML = `<span>${quizNow.likes}</span> likes`
-        }
-        renderQuestions(quizQuestions, answersArray, likes, quizNow)
-    }
-
-    const renderQuestions = (quizQuestions, answersArray, likes, quizNow) => {
-        displayResult.innerHTML = ``
-        valuesArray = []
-
-        // if (displayQuiz.firstElementChild.className === 'null'){
-        //     displayQuiz.firstElementChild.remove()
-        // } 
-        quizQuestions.forEach(question => {
-            const questionLi = document.createElement('li')
-            questionLi.innerHTML = `${question.content}<br>`
-            const questionAnswers = answersArray.filter(answer => answer.question_id == question.id)
-            questionAnswers.forEach(answer => {
-                const questionAnswer = document.createElement('button')
-                questionAnswer.innerText = answer.answer_content
-                questionAnswer.dataset.value = answer.value
-                questionAnswer.className = 'the-questions-answer'
-                questionLi.append(questionAnswer)
-                questionsContainer.append(questionLi)
-            })
+    const getQuestions = (quizNow) => {
+        quizContentContainer.innerHTML = ``;
+        const quizNowQuestions = questionsArray.filter(question => question.quiz_id === quizNow.id);
+        quizNowQuestions.forEach(question => {
+            questionP = document.createElement('p');
+            questionP.textContent = question.content;
+            questionBr = document.createElement('br');
+            questionP.append(questionBr);
+            getAnswers(question, questionP);
         })
-        questionsContainer.append(submitButton)
-        displayQuiz.append(questionsContainer, likeButton, likes)
-        addLikes(likes, quizNow)
     }
 
-    const addLikes = (likes, quizNow) => {
+    const getAnswers = (question, questionP) => {
+        const quizNowQuestionAnswers = answersArray.filter(answer => answer.question_id === question.id);
+        quizNowQuestionAnswers.forEach(answer => {
+            questionAnswer = document.createElement('button');
+            questionAnswer.innerText = answer.answer_content;
+            questionAnswer.className = 'question-answer';
+            questionAnswer.dataset.value = answer.value;
+            questionAnswer.dataset.id = answer.question_id;
+            makeQuestionAnswerDiv(questionAnswer, questionP);
+        })
+    }
+
+    const makeQuestionAnswerDiv = (questionAnswer, questionP) => {
+        questionP.append(questionAnswer);
+        quizContentContainer.append(questionP);
+    }
+
+    const collectValues = () => {
+        document.addEventListener('click', e => {
+            if (e.target.className === 'question-answer' && e.target.nodeName === 'BUTTON') {
+                valuesArray.push(Number(e.target.dataset.value));
+                const answerB = e.target;
+                const answerBParent = answerB.parentNode;
+                const firstButton = answerBParent.firstChild.nextSibling.nextSibling;
+                const secondButton = firstButton.nextSibling;
+                const thirdButton = secondButton.nextSibling;
+                const fourthButton = thirdButton.nextSibling;
+                firstButton.disabled = true;
+                secondButton.disabled = true;
+                thirdButton.disabled = true;
+                fourthButton.disabled = true;
+            }
+        })
+    }
+
+    const submitHandler = () => {
+        submitButton.addEventListener('click', (e) => {
+            let sum = valuesArray.reduce((a, b) => a + b);
+            getResult(sum);
+        })
+    }
+
+    const addLikes = () => {
         likeButton.addEventListener('click', e => {
-            e.preventDefault()
-            let newLikes = (quizNow.likes + 1)
-            // let newLikes = (Number(likes.firstChild.innerText) + 1)
-            fetch(`http://localhost:3000/api/v1/quizzes/${quizNow.id}`, {
+            e.preventDefault();
+            const currentQuiz = quizzesArray.find(quiz => quiz.id == e.target.dataset.id);
+            const newLikes = (Number(likes.innerText) + 1);
+            console.log(newLikes);
+            fetch(`http://localhost:3000/api/v1/quizzes/${currentQuiz.id}`, {
                 method: 'PATCH',
                 headers: {
                     'content-type': 'application/json',
@@ -118,39 +134,13 @@ document.addEventListener("DOMContentLoaded", (e) => {
                 })
             })
             .then(response => response.json())
-            .then(updateLikes(newLikes, likes), console.log(likes))
+            .then(getQuestions(currentQuiz), updateLikes(newLikes))
         })
     }
 
-    const updateLikes = (newLikes, likes) => {
-   likes.innerHTML = ``     
-   likes.innerHTML = `<span>${newLikes}</span> likes`
-    }
-
-    const getValues = () => {
-        document.addEventListener('click', (e) => {
-            if (e.target.className === 'the-questions-answer') {
-                answerB = e.target 
-                valuesArray.push(answerB.dataset.value)
-                answerBParent = answerB.parentNode
-                const firstButton = answerBParent.firstChild.nextSibling.nextSibling 
-                const secondButton = firstButton.nextSibling
-                const thirdButton = secondButton.nextSibling
-                const fourthButton = thirdButton.nextSibling
-                firstButton.disabled = true
-                secondButton.disabled = true
-                thirdButton.disabled = true
-                fourthButton.disabled = true
-            }
-        })
-    }
-
-    const submitHandler = () => {
-        submitButton.addEventListener('click', (e) => {
-            let valuesToSum = valuesArray.map(value => Number(value))
-            let sum = valuesToSum.reduce((a, b) => a + b)
-            getResult(sum)
-        })
+    const updateLikes = (newLikes) => {
+        likes.innerHTML = ``;
+        likes.innerText = newLikes;
     }
 
     const getResult = (sum) => {
@@ -201,15 +191,18 @@ document.addEventListener("DOMContentLoaded", (e) => {
     }
 
     const renderResult = (result) => {
-        const resultH2 = document.createElement('h2')
-        resultH2.innerText = result.title
-        const resultP = document.createElement('p')
-        resultP.innerText = result.description
-        displayResult.append(resultH2, resultP)
+        const resultH2 = document.createElement('h2');
+        resultH2.innerText = result.title;
+        const resultP = document.createElement('p');
+        resultP.innerText = result.description;
+        displayResult.append(resultH2, resultP);
     }
 
-    submitHandler()
     fetchQuizzes();
-    getValues()
-
+    fetchQuestions();
+    fetchAnswers();
+    submitHandler()
+    collectValues()
+    addLikes();
+    
 })
